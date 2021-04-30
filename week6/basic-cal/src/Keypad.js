5953d4edf83157857c6ef37a6905232bcd9f579b
victoriatart <68974738+victoriatart@users.noreply.github.com>
2021-04-29 22:04:01 -0400

    Add files via upload

---
+++ week6/basic-cal/src/Keypad.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import "./Keypad.css"
+
+class Keypad extends Component {
+    render() {
+        return (
+            <div className="Keypad">
+                {this.props.children}
+                
+            </div>
+        );
+    }
+}
+
+export default Keypad;
